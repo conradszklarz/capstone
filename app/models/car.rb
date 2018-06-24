093fab4d9da40b0431c8de26a1283e000fe8fbcc
conradskittles <conradszklarz@gmail.com>
2018-06-24 15:42:32 -0500

    updated

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,4 +4,31 @@ class Car < ApplicationRecord
   has_many :car_parts, through: :maintenance_events
   has_many :car_services
   has_many :services, through: :car_services
+
+  def miles_difference
+    last_oil_change = nil # creates a local variable to start finding last oil change
+
+    maintenance_events.order(:date).each do |maintenance_event|  # loops through each maintenance_event according to date
+      if maintenance_event.oil_change? #  
+        last_oil_change = maintenance_event
+      end
+    end
+
+    difference = mileage - last_oil_change.mileage
+    difference > 4000
+  end
+
+  def months_difference
+    last_oil_change = nil
+    
+
+
+    
+  end
+
+  def oil_change_alert?
+    
+  end
 end
+
+
--- app/models/maintenance_event.rb
+++ app/models/maintenance_event.rb
@@ -3,4 +3,8 @@ class MaintenanceEvent < ApplicationRecord
   has_many :car_parts
   has_many :maintenance_services
   has_many :services, through: :maintenance_services
+
+  def oil_change?
+    services.select {|service| service.name == "Oil Change" }.any?
+  end
 end
