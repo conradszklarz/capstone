43c41b194e440b5be3edcc0abb4bfe8147f8d182
conradskittles <conradszklarz@gmail.com>
2018-07-01 16:46:41 -0500

    fixed bootstrap

--- app/models/car.rb
+++ app/models/car.rb
@@ -17,7 +17,7 @@ class Car < ApplicationRecord
     if last_oil_change
       difference = mileage - last_oil_change.mileage 
       difference > 4000
-    else
+    else                  #not based on hardcoded oil change but any service
       false
     end
   end
@@ -34,24 +34,24 @@ class Car < ApplicationRecord
     if last_oil_change
       last_oil_change.date < 4.months.ago
     else
-      created_at < 4.months.ago
+      created_at < 4.months.ago #look at eval() METHOD!!
     end
   end
     
 
 
-  def oil_change_alert?
+  def oil_change_alert?()
     miles_difference || months_difference
   end
 
-  def alerts
+  def alerts #pass in all services for this car
     alerts_collection = []
 
     service_to_check = Service.find(1)#tires
     service_to_check = Service.find(2)#oil change
 
     if oil_change_alert?
-      alerts_collection << "Time to change your oil and oil filter. For the do-it-yourselfer, also, please remember to blow out engine air filter, cabin air filter, top off radiator and radiator reservoir, top off brake fluid tank, grease all grease joints, check tire pressure, and check for any leaks under the car."
+      alerts_collection << "Time to change your oil and oil filter. ***For the do-it-yourselfer...please remember to blow out engine air filter, cabin air filter, top off radiator and radiator reservoir, top off brake fluid tank, grease all grease joints, check tire pressure, and check for any leaks under the car."
     end
 
     alerts_collection
